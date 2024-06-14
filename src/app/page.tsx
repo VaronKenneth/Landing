310a3125971e0d4e387226542d8a8ad0be9e7ee6
Kafazevc <alejandrovkc@gmail.com>
2024-06-14 10:31:47 -0500

    Landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,51 @@
+import Testimonials from "@/components/testimonial";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <section>
+        <div className="w-full h-[100%] bg-[#808080]">
+            <div className="w-full h-[100px] flex items-center justify-center">
+              <Image
+                    src="/image.png"
+                    alt="logo"
+                    width={100}
+                    height={100} 
+                    className="w-[100px] h-[100px]"
+                />
+            </div>
+            <div className="flex items-center justify-end pr-[100px] pt-4 pb-[52.5px]">
+              <div className="w-[35%] h-[450px] bg-white rounded-2xl">
+                <h1 className="flex justify-center pt-3 text-lg"><b>COTIZA CON NOSOTROS</b></h1>
+                  <form action="Form" className="flex flex-col items-center pt-2 gap-3">
+                    <input type="text" className="h-[50px] w-[90%] bg-[#E0E0E0] rounded-lg pl-3"/>
+                    <input type="email" className="h-[50px] w-[90%] bg-[#E0E0E0] rounded-lg pl-3"/>
+                    <textarea className="h-[160px] w-[90%] bg-[#E0E0E0] rounded-lg pl-3 pt-3 resize-none"/>
+                    <label htmlFor="Check">
+                      <input type="checkbox" name="Terms and conditions"/> He leido los <b>Terminos y Condiciones</b>
+                    </label>
+                    <input type="submit" name="submit" value=" "  className="h-[50px] w-[90%] bg-[#E0E0E0] rounded-lg"/>
+                  </form>
+              </div>
+            </div>
         </div>
-      </div>
+      </section>
+      <section>
+        <div className="w-full h-[400px] bg-[#CCCCCC]">
+          <div className="relative pt-12 ml-14">
+              <div className="absolute top-1/3 h-[240px] w-[35%] bg-[#666666] pl-10 rounded-lg">
+                
+              </div>
+              <div className="relative h-[250px] w-[35%] bg-[#E6E6E6] ml-8 mt-4 rounded-lg">
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <Testimonials /> 
+      </section>
     </main>
   );
 }
---
+++ src/components/testimonial.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import { QuotationMarks } from "./icons"
+
+export default function Testimonials() {
+    return (
+        <section>
+            <div className="w-full h-[400px]">
+                <div className="w-[60%] h-[350px] ">
+
+                </div>
+            </div>
+        </section>
+    );
+};
